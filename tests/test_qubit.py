faddb24d66794bb031d58303252828c9f2a4139a
Virginia <virginia.m.frey@gmail.com>
2018-12-30 17:02:48 +1100

    added test templates for qubit_register

---
+++ tests/test_qubit.py
@@ -0,0 +1,15 @@
+"""
+Testing the qubit register class
+"""
+
+def test_qubit_initialisation():
+    pass
+
+def test_qubit_measurement():
+    pass
+
+def test_qubit_set_state():
+    pass
+
+def test_qubit_unitary_rotation():
+    pass
