60c3a448a4ee1edc02e3cb985894184e814c8e22
Alan Robertson <arob8086@uni.sydney.edu.au>
2018-12-30 16:52:42 +1100

    Setting up qubit template

---
+++ two_qubit_simulator/qubit_register.py
@@ -0,0 +1,26 @@
+# Put docstrings here
+
+
+class QubitRegister(object):
+    #
+    #
+    #
+
+    def __init__(self, initial_state):
+        pass
+        
+
+    def measure(self, basis):
+        pass
+
+    def apply_unitary(self, unitary):
+        pass    
+
+    def set_state(self, state):
+        pass
+
+    def _calculate_density_matrix_(self):
+        pass
+
+    def __repr__(self):
+        print(self)
