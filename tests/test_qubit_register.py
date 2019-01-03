50eaa596df7d0fa8db6b20678215823418a3695c
Virginia <virginia.m.frey@gmail.com>
2019-01-03 20:24:12 +1100

    started writing tests

--- tests/test_qubit_register.py
+++ tests/test_qubit_register.py
@@ -1,9 +1,25 @@
 """
 Testing the qubit register class
 """
+import numpy as np
+from two_qubit_simulator import QubitRegister
 
 def test_qubit_initialisation():
-    pass
+    # Single qubit state
+    qr_1 = QubitRegister([0, 1])
+    assert np.allclose(qr_1.state, np.array([[0, 0], [0, 1]]))
+    assert qr_1.n_qubits == 1
+    # Two qubit state
+    qr_2 = QubitRegister([0, 0, 0, 1])
+    assert np.allclose(qr_2.state, np.array([
+        [0, 0, 0, 0],
+        [0, 0, 0, 0],
+        [0, 0, 0, 0],
+        [0, 0, 0, 1]
+    ]).astype(np.complex128))
+    assert qr_2.n_qubits == 2
+
+
 
 def test_qubit_measurement():
     pass
