682b5a69c665e0fb8c1f3fbb23620550920794a4
EQUSPhysics <equsp@internal.usyd.edu.au>
2019-01-30 14:59:29 +1100

    Fixed spelling error

--- two_qubit_simulator/__init__.py
+++ two_qubit_simulator/__init__.py
@@ -1,6 +1,6 @@
 """
 # __init__.py
 Initialise the two_qubit_simulator module.
-Add import statements from auxilirary modules here.
+Add import statements from auxiliary modules here.
 """
 from .utilities import conjugate_transpose
