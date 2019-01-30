c0f8a22a2aef114d332452752e33c5874e9240c9
kp-trs <08karun.paul@gmail.com>
2019-01-30 16:24:29 +1100

    Added attributes to Register class. Added ability to apply unitary operators

--- two_qubit_simulator/qubit_register.py
+++ two_qubit_simulator/qubit_register.py
@@ -4,17 +4,32 @@ Contains the QubitRegister class
 
 class QubitRegister(object): # pylint: disable=useless-object-inheritance
     """ Defines a qubit register with one or two qubits.
-        
-        - - - WRITE DOCUMENTATION HERE - - -
+        All members of the register class must be arrrys of shape (2,2) or (2,1)
+        Non-normalised states can be initialised but will raise warnings
+
+        Registers objects may have unitary operators assigned to them.
+        Operators must be of shape (2,2)
+        Operators must be normalised
+
     """
 
     def __init__(self, initial_state):
-        """ Create a QubitRegister object """
-        pass
+        self.dimension = initial_state.shape
+        self.state = initial_state
+        self.is_normalised = np.linalg.norm(self.state) == 1
+        
+        if not self.is_normalised:
+            print('Warning! State not normalised!')
+        assert self.dimension ==  (2,2) or self.dimension ==  (2,1)  , " Please enter a 1 or 2 qubit state"
+
+        
 
     def apply_unitary(self, unitary):
         """ Apply a unitary state transformation on the qubit register """
-        pass
+        assert unitary @ unitary.conj().T == np.array([[1,0],[0,1]]), "Operator not unitary!"
+        output =  unitary @ self.initial_state
+        return output
+        
 
     def measure(self, number_of_samples=1):
         """ Perform a measurement of the qubit register by sampling from a uniform
@@ -22,3 +37,9 @@ class QubitRegister(object): # pylint: disable=useless-object-inheritance
         """
         pass
 
+
+
+
+state1 = QubitRegister(np.array([[1,0],[0,1]]))
+hadamard1 = Hadamard(2)
+state.apply_unitary(hadamard1)
